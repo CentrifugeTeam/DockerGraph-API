848d6c940d95678e9dc21f574e176cff7de699c1
mainStorne <dbevzpr@mail.ru>
2025-04-07 16:19:07 +0000

    fix plantuml

--- api/modules/export/__init__.py
+++ api/modules/export/__init__.py
@@ -9,20 +9,17 @@ from ..graph.manager import graph_manager
 r = APIRouter(prefix='/export')
 
 
-@r.get('/uml', response_class=StreamingResponse)
+@r.get('/plantuml', response_class=StreamingResponse)
 async def plantuml(session: Session):
 
     nodes, links = await graph_manager.get_graph(session)
     buffer = StringIO()
-    buffer.write("@startuml\n")
-    for node in nodes:
-        buffer.write(f'object "{node.name}" as {node.id}\n')
+    buffer.write("@startmindmap\ntitle Docker Network\n")
+    hashmap = {node.id: node for node in nodes}
     for link in links.values():
-        first = link[0]
-        for l in link[1:]:
-            buffer.write(f'{first} -> {l}\n')
-            first = l
-    buffer.write("@enduml\n")
+        for i, l in enumerate(link):
+            buffer.write(f"{(i+1) * '*'} {hashmap[l].name}\n")
+    buffer.write("@endmindmap\n")
     buffer.seek(0)
 
     return StreamingResponse(buffer, media_type="text/plain",
