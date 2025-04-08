d1b15a9931e8ff5db1b05d91044ef91260f6dcdc
mainStorne <dbevzpr@mail.ru>
2025-04-08 11:26:26 +0000

    byd

--- api/modules/export/json.py
+++ api/modules/export/json.py
@@ -1,5 +1,4 @@
 from io import StringIO
-from uuid import UUID
 
 import networkx as nx
 from fastapi import APIRouter
@@ -9,43 +8,50 @@ from ...db import Network
 from ...deps import Session
 from ..graph.manager import graph_manager
 
-r = APIRouter(prefix='/export')
+r = APIRouter(prefix='/json')
 
 
-def create_mindmap_from_networks(buffer: StringIO, networks: list[Network], start='**'):
-    container_start = '*' * (len(start) + 1)
+def create_mindmap_from_networks(networks: list['Network'], data: dict):
+    data['networks'] = []
     for network in networks:
-        buffer.write(f"{start} Network: {network.name}\n")
-        for container in network.containers:
-            buffer.write(f"{container_start} Container: {container.name}\n")
+        containers = [network.model_dump(exclude={'containers'}) for network in network.containers]
+        data['networks'].append(network.model_dump(exclude={''}))
+        
+        
+        # buffer.write(f"{start} Network: {network.name}\n")
+        # for container in network.containers:
+        #     buffer.write(f"{container_start} Container: {container.name}\n")
 
 
-@r.get('/plantuml', response_class=StreamingResponse)
-async def plantuml(session: Session, id: UUID | None = None):
-    # if 
+@r.get('', response_class=StreamingResponse)
+async def plantuml(session: Session):
+
     nodes, edges = await graph_manager.get_graph(session)
     graph = nx.Graph()
     graph.add_nodes_from(
         [(node.id, {'name': node.hostname, "networks": node.networks}) for node in nodes])
     graph.add_edges_from(
         [(edge.source_host_id, edge.target_host_id) for edge in edges])
-    buffer = StringIO()
-    buffer.write("@startmindmap\ntitle Docker Network\n")
+    response = {'title': 'Docker Network', 'hosts': []}
+    hosts = response['hosts']
     visited = set()
-    for i, node in enumerate(graph.nodes()):
+    for node in graph.nodes():
         if node in visited:
             continue
-        buffer.write(f"* MainHost: {graph.nodes[node]['name']}\n")
+        host = {'name': graph.nodes[node]['name'],  'neighbors': []}
+        neighbors = host['neighbors']
         for neighbor in graph.neighbors(node):
             if neighbor in visited:
                 continue
-            buffer.write(f"** NeighborHost: {graph.nodes[neighbor]['name']}\n")
+            neighbors.append({'name': graph.nodes[neighbor]['name']})
             create_mindmap_from_networks(
-                buffer, graph.nodes[neighbor]['networks'], start='***')
+                graph.nodes[neighbor]['networks'], neighbor)
+            
             visited.add(neighbor)
 
         create_mindmap_from_networks(
             buffer, graph.nodes[node]['networks'], start='**')
+        hosts.append(host)
         visited.add(node)
 
     buffer.write("@endmindmap\n")
